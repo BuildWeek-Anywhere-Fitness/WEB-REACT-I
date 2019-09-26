84f417b74937a098dfd83b23ca68aa8652fb03da
oladimeji-ojo <ojokuredim@gmail.com>
2019-09-26 18:32:23 +0100

    styling

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UserForm from "./SignInForm";
 import SignUpForm from "./SignUpForm";
 import Axios from "axios";
